c39d9a3be0c9f699901dac777c8667aa09887d6e
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-28 07:43:23 -0400

    Implemented the main menu screen

--- src/Menu.java
+++ src/Menu.java
@@ -1,22 +1,66 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Menu {
-    public final static void clearConsole() throws InterruptedException, IOException {
-        new ProcessBuilder("clear").inheritIO().start().waitFor();
+    public static void printMenu() {
+        System.out.println("========================================");
+        System.out.println("==========   MENU PRINCIPAL   ==========");
+        System.out.println("========================================");
+        System.out.println("Connected to database " + Database.getDbName());
+        System.out.println();
+        System.out.println("[ 1 ] CRIAR NOVO USUÁRIO");
+        System.out.println("[ 2 ] ENTRAR");
+        System.out.println("[ 3 ] SAIR");
+        System.out.println();
+        System.out.print("Selecione a opção desejada: ");
 
+        Scanner sc = Main.getScanner();
+        int menuOp = sc.nextInt();
+        switch (menuOp) {
+            case 1:
+                clear();
+                new CreateNewUser();
+                prosseguir();
+                break;
+            case 2:
+                clear();
+                new Login();
+                prosseguir();
+                break;
+            case 3:
+                Main.finalizar();
+                break;
+        
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+                System.out.println();
+                prosseguir();
+                clear();
+                printMenu();
+        }
     }
 
-    public static void apagarConsole() {
+    public static void clear() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             }
             else {
-                System.out.print("\033\143");
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
             }
         } catch (IOException | InterruptedException e) {
             System.out.println(e.getMessage());
             System.out.println(e.getStackTrace());
+        }
+    }
+
+    public static void prosseguir() {
+        try {
+            System.out.println();
+            System.out.println("Pressione [ENTER] para prosseguir ...");
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
