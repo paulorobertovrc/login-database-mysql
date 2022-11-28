6214824c28797e6553b8dc2638e668905008639a
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-28 09:59:46 -0400

    This class will generate the hash to be stored in the db

---
+++ src/GenerateHash.java
@@ -0,0 +1,8 @@
+
+public class GenerateHash {
+    public static String generateHash(String password) {
+        System.out.println("Gerando hash da senha " + password);
+        String passwordHash = password;
+        return passwordHash;
+    }
+}
