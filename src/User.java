fee5dd06aee6dd96622faa7a01fed1f5ea395393
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-28 10:34:57 -0400

    User class template

--- src/User.java
+++ src/User.java
@@ -1,12 +1,33 @@
 public abstract class User {
     private String login = "";
     private String password = "";
+    private static boolean isAdmin = false;
+    
+    public static void setAdmin() {
+        User.isAdmin = true;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     User(String login) {
         this.login = login;
     }
 
     public void listUsers() {
-        System.out.println("teste do método listUsers da classe User");
     }
 }
