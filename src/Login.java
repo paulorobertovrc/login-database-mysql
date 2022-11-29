bec81fbf6903a2c92b3da2996112acb43821a7fd
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-29 08:53:47 -0400

    Handles login

---
+++ src/Login.java
@@ -0,0 +1,46 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Login {
+    private String loggingUser = "";
+    private String password = "";
+    
+    Login() {
+        System.out.print("Nome do usuário: ");
+        Scanner sc = Main.getScanner();
+        this.loggingUser = sc.next();
+        System.out.print("Senha: ");
+        this.password = sc.next();
+
+        System.out.println("Informado: " + loggingUser);
+
+        if (!(this.loggingUser.isBlank()) && !(this.password.isBlank())) {
+            int id;
+            String username = "";
+            String password = "";
+            int isAdmin;
+
+            try {
+                ResultSet rs = Query.fetchUser(loggingUser);
+                rs.next();
+
+                do {
+                    id = rs.getInt("id");
+                    username = rs.getString("username");
+                    password = rs.getString("password");
+                    isAdmin = rs.getInt("isAdmin");
+                } while (rs.next() && !loggingUser.equals(username));
+                
+                if (loggingUser.equals(username) && this.password.equals(password)) {
+                    new InstantiateUser(id, username, isAdmin);
+                } else {
+                    System.out.println("Acesso negado. Verifique suas credenciais e tente novamente.");
+                    Main.finalizar();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
