a6a3b2bcde89829de2621f2652ad37544eaac20b
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-30 08:46:18 -0400

    Implementation of the hashing function

--- src/GenerateHash.java
+++ src/GenerateHash.java
@@ -1,8 +1,22 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class GenerateHash {
+    private static String hashedPassword = "";
+    
     public static String generateHash(String password) {
-        System.out.println("Gerando hash da senha " + password);
-        String passwordHash = password;
-        return passwordHash;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] mdByteArray = md.digest(password.getBytes());
+            BigInteger bigInt = new BigInteger(mdByteArray);
+            hashedPassword = bigInt.toString(16);
+
+            return hashedPassword;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return hashedPassword;
     }
 }
