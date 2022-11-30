4b976fac7866277416aac28ed3b7b4232c95f9ae
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-11-30 09:36:22 -0400

    This class is responsible to call the Query.listUsersQuery() method

---
+++ src/ListUsers.java
@@ -0,0 +1,11 @@
+
+public class ListUsers {
+    public ListUsers() {
+        System.out.println("=============================================");
+        System.out.println("============   LISTAR USUÁRIOS   ============");
+        System.out.println("=============================================");
+        System.out.println();
+
+        Query.listUsersQuery();
+    }
+}
