f8c816749333cbdf2b4540c4970e953284f553d5
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-01 10:57:43 -0400

    Updated ListUser screen

--- src/ListUsers.java
+++ src/ListUsers.java
@@ -1,5 +1,7 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListUsers {
     public ListUsers() {
@@ -8,17 +10,36 @@ public class ListUsers {
         System.out.println("=============================================");
         System.out.println();
 
-        // Query.listUsersQuery();
-    }
-
-    private String drawTable(String[][] table) {
-        String borderRow = Arrays.stream(table[0])
-                .map(str -> "-".repeat(str.length()))
-                .collect(Collectors.joining("+", "+", "+\n"));
+        List<User> userList = new ArrayList<>();
         
-        return Arrays.stream(table)
-                .map(row -> Arrays.stream(row)
-                .collect(Collectors.joining("|", "|", "|\n")))
-                .collect(Collectors.joining(borderRow, borderRow, borderRow));
+        try {
+            int id = 0;
+            String username = "";
+            int isAdmin = 0;
+            
+            ResultSet rs = Query.listUsersQuery();
+            rs.next();           
+
+            do {
+                id = rs.getInt("id");
+                username = rs.getString("username");
+                isAdmin = rs.getInt("isAdmin");
+                User usr = null;
+                if (isAdmin == 0) {
+                    usr = new CommonUser(id, username);
+                    userList.add(usr);
+                } else {
+                    usr = new AdminUser(id, username);
+                    userList.add(usr);
+                }
+            } while (rs.next());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("+--- ID ---+---------- USERNAME ------------+");
+        for (User user : userList) {
+            System.out.println("|    " + user.getId() + "     | " + user.getLogin());
+        }      
     }
 }
